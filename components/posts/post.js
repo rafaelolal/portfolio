d35e669ac50bb1a2e026b4b7e4ab78d7c409bea8
Rafael Almeida <rafaelpbcp@gmail.com>
2022-11-08 19:46:11 -0500

    added key to .map

--- components/posts/post.js
+++ components/posts/post.js
@@ -55,12 +55,12 @@ export default function Post(props) {
 
         <p className="fw-bold fs-3 mb-0">{props.title}</p>
         <div style={{ maxHeight: 200, overflow: "auto" }}>
-          {descriptionList.map((paragraph) => (
-            <p className="tab">{paragraph}</p>
+          {descriptionList.map((paragraph, i) => (
+            <p key={i} className="tab">{paragraph}</p>
           ))}
           <p>
-            {props.links.map((link) => (
-              <a className="me-2" href={link}>{link}</a>
+            {props.links.map((link, i) => (
+              <a key={i} className="me-2" href={link}>{link}</a>
             ))}
           </p>
         </div>
