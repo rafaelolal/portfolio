4cc73acdc32271a509888263c66be1ffc6f49692
Rafael Almeida <rafaelpbcp@gmail.com>
2024-09-25 02:22:49 +0000

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,18 @@ export default function Document() {
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
           strategy="beforeInteractive"
         />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-LN60K0MXP5"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-LN60K0MXP5');
+              `,
+            }}
+          />
+
       </Head>
       <body>
         <Main />
